4f09f0a4852b39620cb63569146e027a0e48fae2
Wcb-2000 <846583697@qq.com>
2021-03-04 08:32:54 +0800

    简化url代码段

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,19 +1,26 @@
-  // 引入 用来发送请求的方法 一定要把路径补全
-  import {request} from "../../request/index.js"
-  Page({
-    data: {
-      //轮播图数组
-      swiperList:[],
-      //导航 数组
-      catesList:[],
-      // 楼层数据
-      floorList:[],
-      
-    },
-    //options(Object)
-    //页面开始加载就会触发
-    onLoad: function(options){
-      // 1 发送异步请求获取轮播图数据
+/*
+ * Author: 吴楚标
+ * Date: 2021-02-23 21:01:57
+ * LastEditors: 吴楚标
+ * LastEditTime: 2021-03-03 23:27:39
+ * Description: 
+*/
+// 引入 用来发送请求的方法 一定要把路径补全
+import { request } from "../../request/index.js"
+Page({
+  data: {
+    //轮播图数组
+    swiperList: [],
+    //导航 数组
+    catesList: [],
+    // 楼层数据
+    floorList: [],
+
+  },
+  //options(Object)
+  //页面开始加载就会触发
+  onLoad: function (options) {
+    // 1 发送异步请求获取轮播图数据
     //  wx.request({
     //     url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
     //     // data: {},  不用发送返回值可删
@@ -25,37 +32,36 @@
     //       this.setData({
     //         swiperList:result.data.message
     //       })
-          this.getSwiperList();
-          this.getcatesList(); 
-          this.getfloorList();
+    this.getSwiperList();
+    this.getcatesList();
+    this.getfloorList();
 
-      
-    },
-    //获取轮播图数据
-    getSwiperList(){
-      request({url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata'})
-      .then(result=>{
+
+  },
+  //获取轮播图数据
+  getSwiperList() {
+    request({ url: '/home/swiperdata' })
+      .then(result => {
         this.setData({
-          swiperList:result.data.message
+          swiperList: result
+        })
       })
-    })
-    },
-    //获取导航栏数据
-    getcatesList(){
-      request({url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/catitems'})
-      .then(result=>{
+  },
+  //获取导航栏数据
+  getcatesList() {
+    request({ url: '/home/catitems' })
+      .then(result => {
         this.setData({
-          catesList:result.data.message
+          catesList: result
+        })
       })
-    })
-    },
-    getfloorList(){
-      request({url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/floordata'})
-      .then(result=>{
+  },
+  getfloorList() {
+    request({ url: '/home/floordata' })
+      .then(result => {
         this.setData({
-          floorList:result.data.message
+          floorList: result
+        })
       })
-    })
-    },
-   
+  },
   });
